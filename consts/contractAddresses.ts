53ee585461285551657fd3d4daf0a9a4de7e133b
Ahjin <122112202+Officialahjin@users.noreply.github.com>
2023-03-16 08:34:45 +0800

    first commit

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0x636f3acB0661656B390abC54Cf45d8424540B905";
+export const tokenContractAddress =
+  "0xfCb5A415c5665a2868e9afc776fB506E127fD373";
+export const stakingContractAddress =
+  "0x436557C49cE058076E813772101bFB2B57578C36";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,157 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import {
+  ConnectWallet,
+  ThirdwebNftMedia,
+  useAddress,
+  useContract,
+  useContractRead,
+  useOwnedNFTs,
+  useTokenBalance,
+  Web3Button,
+} from "@thirdweb-dev/react";
+import { BigNumber, ethers } from "ethers";
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import NFTCard from "../components/NFTCard";
+import {
+  nftDropContractAddress,
+  stakingContractAddress,
+  tokenContractAddress,
+} from "../consts/contractAddresses";
 import styles from "../styles/Home.module.css";
+import React from 'react'
 
-const Home: NextPage = () => {
+const Stake: NextPage = () => {
+  const address = useAddress();
+  const { contract: nftDropContract } = useContract(
+    nftDropContractAddress,
+    "nft-drop"
+  );
+  const { contract: tokenContract } = useContract(
+    tokenContractAddress,
+    "token"
+  );
+  const { contract, isLoading } = useContract(stakingContractAddress);
+  const { data: ownedNfts } = useOwnedNFTs(nftDropContract, address);
+  const { data: tokenBalance } = useTokenBalance(tokenContract, address);
+  const [claimableRewards, setClaimableRewards] = useState<BigNumber>();
+  const { data: stakedTokens } = useContractRead(
+    contract,
+    "getStakeInfo",
+    address
+  );
+
+  useEffect(() => {
+    if (!contract || !address) return;
+
+    async function loadClaimableRewards() {
+      const stakeInfo = await contract?.call("getStakeInfo", address);
+      setClaimableRewards(stakeInfo[1]);
+    }
+
+    loadClaimableRewards();
+  }, [address, contract]);
+
+  async function stakeNft(id: string) {
+    if (!address) return;
+
+    const isApproved = await nftDropContract?.isApproved(
+      address,
+      stakingContractAddress
+    );
+    if (!isApproved) {
+      await nftDropContract?.setApprovalForAll(stakingContractAddress, true);
+    }
+    await contract?.call("stake", [id]);
+  }
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+ 
+
   return (
+    
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
-        </h1>
+      <h1 className={styles.h1}>Stake Your Ahjin Labs NFTs</h1>
+      <hr className={`${styles.divider} ${styles.spacerTop}`} />
 
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
-        </p>
+      {!address ? (
+        <ConnectWallet
+        colorMode="light"
+        accentColor="#C994DF"
+         />
+      ) : (
+        <>
+        <div className={styles.header}></div>
+      <img className={styles.logoIcon} alt="" src="/logo.svg" />
+          <h2>Your Tokens</h2>
+          <div className={styles.tokenGrid}>
+            <div className={styles.tokenItem}>
+              <h3 className={styles.tokenLabel}>Claimable Rewards</h3>
+              <p className={styles.tokenValue}>
+                <b>
+                  {!claimableRewards
+                    ? "Loading..."
+                    : ethers.utils.formatUnits(claimableRewards, 18)}
+                </b>{" "}
+                {tokenBalance?.symbol}
+              </p>
+            </div>
+            <div className={styles.tokenItem}>
+              <h3 className={styles.tokenLabel}>Current Balance</h3>
+              <p className={styles.tokenValue}>
+                <b>{tokenBalance?.displayValue}</b> {tokenBalance?.symbol}
+              </p>
+            </div>
+          </div>
 
-        <div className={styles.connect}>
-          <ConnectWallet />
-        </div>
-
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
+          <Web3Button
+            action={(contract) => contract.call("claimRewards")}
+            contractAddress={stakingContractAddress}
+            colorMode="light"
+            accentColor="#C994DF"
           >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
-      </main>
+            Claim $AHJ
+          </Web3Button>
+
+          <hr className={`${styles.divider} ${styles.spacerTop}`} />
+          <h2>Your Staked NFTs</h2>
+          <div className={styles.nftBoxGrid}>
+            {stakedTokens &&
+              stakedTokens[0]?.map((stakedToken: BigNumber) => (
+                <NFTCard
+                  tokenId={stakedToken.toNumber()}
+                  key={stakedToken.toString()}
+                />
+              ))}
+          </div>
+
+          <hr className={`${styles.divider} ${styles.spacerTop}`} />
+          <h2>Your Unstaked NFTs</h2>
+          <div className={styles.nftBoxGrid}>
+            {ownedNfts?.map((nft) => (
+              <div className={styles.nftBox} key={nft.metadata.id.toString()}>
+                <ThirdwebNftMedia
+                  metadata={nft.metadata}
+                  className={styles.nftMedia}
+                />
+                <h3>{nft.metadata.name}</h3>
+                <Web3Button
+                  contractAddress={stakingContractAddress}
+                  action={() => stakeNft(nft.metadata.id)}
+                  colorMode="light"
+                  accentColor="#C994DF"
+                >
+                  Stake
+                </Web3Button>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
 
-export default Home;
+export default Stake;
